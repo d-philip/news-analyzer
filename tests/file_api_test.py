b2b9ae7a2fd538dcb4b4e83e9291ada32c5290d2
d-philip <djphilip@bu.edu>
2021-03-31 19:35:57 -0400

    Added test cases to file API POST test.

--- tests/file_api_test.py
+++ tests/file_api_test.py
@@ -12,16 +12,34 @@ def test_post_file():
                   "last_name": "Doe",
                   "occupation": "baker"
                 }
-    resp1 = r.post(user_api_url, json=new_user)
+    r.post(user_api_url, json=new_user)
 
     # formate request data
-    document = 'tests/Salmon and Couscous Salad Recipe - NYT Cooking.pdf'
-    files = {'file': open(document, 'rb'), 'source': (None, 'disk')}
+    document1 = 'tests/Salmon and Couscous Salad Recipe - NYT Cooking.pdf'
+    files1 = {'file': open(document1, 'rb'), 'source': (None, 'disk')}
+    document2 = 'tests/applesauce.txt'
+    files2 = {'file': open(document2, 'rb'), 'source': (None, 'disk')}
+    files3 = {'source': (None, 'disk')}
+    files4 = {'file': open(document2, 'rb')}
 
-    resp2 = r.post(file_api_url + 'johndoe@gmail.com/files/', files=files)
+    resp2 = r.post(file_api_url + 'johndoe@gmail.com/files/', files=files1)
     assert resp2.json() == {'response': 'File uploaded successfully'}
     assert resp2.status_code == 200
 
-    resp3 = r.post(file_api_url + 'janedoe@gmail.com/files/', files=files)
+    resp3 = r.post(file_api_url + 'janedoe@gmail.com/files/', files=files1)
     assert resp3.json() == {'error': 'User could not be found.'}
     assert resp3.status_code == 404
+
+    resp4 = r.post(file_api_url + 'johndoe@gmail.com/files/', files=files2)
+    assert resp4.json() == {'error': 'File type not supported.'}
+    assert resp4.status_code == 400
+
+    resp5 = r.post(file_api_url + 'johndoe@gmail.com/files/', files=files3)
+    assert resp5.json() == {'error': 'No file sent with the request.'}
+    assert resp5.status_code == 400
+
+    resp6 = r.post(file_api_url + 'johndoe@gmail.com/files/', files=files4)
+    assert resp6.json() == {'error': 'No source sent with the request.'}
+    assert resp6.status_code == 400
+
+    r.delete(user_api_url + 'johndoe@gmail.com')
